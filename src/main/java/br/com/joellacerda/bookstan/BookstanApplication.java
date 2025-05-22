b661d12dad4537efa9aa1162c83638ae117cc3a4
trrrrryuttttt <joellacerdaol@gmail.com>
2025-05-22 10:02:54 -0300

    Initial commit

---
+++ src/main/java/br/com/joellacerda/bookstan/BookstanApplication.java
@@ -0,0 +1,13 @@
+package br.com.joellacerda.bookstan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joellacerda/bookstan/BookstanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joellacerda.bookstan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
